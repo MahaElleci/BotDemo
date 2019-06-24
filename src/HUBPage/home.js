b15cdfbeec5feaf259a68b391fce3922e037cd09
alyameen <a7mednazih1994@gmail.com>
2019-06-24 16:10:27 +0200

    first commit

---
+++ src/HUBPage/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './home.css'
+const Home = () => {
+    return (
+        <div className="wrapper">
+            <img className="hubLogo" src="http://www.itworxhub.com/assets/images/logo-colored.png"/>
+        </div>
+    );
+};
+
+export default Home;
